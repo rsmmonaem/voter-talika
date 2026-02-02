49521dd1b70c9bf0c7a9d8b5da3ad93f987ffd96
rsmmonaem <vrkm55@gmail.com>
2026-02-02 22:09:57 +0600

    Update default port to 7860 for Hugging Face

--- voter-web/server.js
+++ voter-web/server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 7860;
 const DB_PATH = path.join(__dirname, 'voters.db');
 
 app.use(cors());
